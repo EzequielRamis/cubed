85cb5d1ece0e3d55998ceae8510fd09d8f460625
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2020-09-03 18:10:26 -0300

    first relative rotation

--- src/system/input.rs
+++ src/system/input.rs
@@ -9,12 +9,24 @@ pub fn keyboard_input(
     mut query: Query<(&Piece, &mut Rotation, &mut Translation)>,
 ) {
     for (_, mut rotation, mut translation) in &mut query.iter() {
-        if input.just_pressed(KeyCode::W) {}
-        if input.just_pressed(KeyCode::A) {}
-        if input.just_pressed(KeyCode::S) {}
-        if input.just_pressed(KeyCode::D) {}
-        if input.just_pressed(KeyCode::Q) {}
-        if input.just_pressed(KeyCode::E) {}
+        if input.just_pressed(KeyCode::W) {
+            rotation.0 *= Quat::from_rotation_x(-FRAC_PI_2);
+        }
+        if input.just_pressed(KeyCode::A) {
+            rotation.0 *= Quat::from_rotation_z(FRAC_PI_2);
+        }
+        if input.just_pressed(KeyCode::S) {
+            rotation.0 *= Quat::from_rotation_x(FRAC_PI_2);
+        }
+        if input.just_pressed(KeyCode::D) {
+            rotation.0 *= Quat::from_rotation_z(-FRAC_PI_2);
+        }
+        if input.just_pressed(KeyCode::Q) {
+            rotation.0 *= Quat::from_rotation_y(FRAC_PI_2);
+        }
+        if input.just_pressed(KeyCode::E) {
+            rotation.0 *= Quat::from_rotation_y(-FRAC_PI_2);
+        }
         if input.just_pressed(KeyCode::I) {
             translation.0 -= Vec3::unit_z();
         }
